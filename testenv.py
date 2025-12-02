da25ecb4e32ad9edf34e2d9c66995508f0d476ad
Alexey Rybalchenko <alexryba@gmail.com>
2025-12-02 12:17:15 +0100

    Fix checkenv and testenv

--- testenv.py
+++ testenv.py
@@ -1,7 +1,36 @@
 from environment import ComputeClusterEnv
+from weights import Weights
 
-env = ComputeClusterEnv()
-env.render()
+weights = Weights(
+    efficiency_weight=0.7,
+    price_weight=0.2,
+    idle_weight=0.1,
+    job_age_weight=0.0
+)
+
+env = ComputeClusterEnv(
+    weights=weights,
+    session='test',
+    render_mode='human',
+    quick_plot=True,
+    external_prices=None,
+    external_durations=None,
+    external_jobs=None,
+    external_hourly_jobs=None,
+    plot_rewards=False,
+    plots_dir='sessions/test/plots',
+    plot_once=False,
+    plot_eff_reward=False,
+    plot_price_reward=False,
+    plot_idle_penalty=False,
+    plot_job_age_penalty=False,
+    skip_plot_price=False,
+    skip_plot_online_nodes=False,
+    skip_plot_used_nodes=False,
+    skip_plot_job_queue=False,
+    steps_per_iteration=100000,
+    evaluation_mode=False
+)
 episodes = 1
 
 for episode in range(episodes):
